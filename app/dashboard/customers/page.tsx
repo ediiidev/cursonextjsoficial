1c4983c40784138df31de436dcde7ff7fc9220d7
Ediii Dev <ediii.dev@gmail.com>
2026-05-21 13:30:49 -0400

    feat: Metadatos

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,4 +1,10 @@
 import React from "react";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Customers",
+  description: "View and manage your customers on the My Financial Dashboard.",
+};
 
 export default function Customers() {
   return <div>customers</div>;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,16 @@
 import "./ui/global.css";
 import { inter } from "@/app/ui/fonts";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: {
+    template: "%s | My Financial Dashboard",
+    default: "My Financial Dashboard",
+  },
+  description:
+    "A simple financial dashboard built with Next.js 13 and TypeScript.",
+  metadataBase: new URL("https://next-learn-dashboard.vercel.sh"),
+};
 
 export default function RootLayout({
   children,
